6074c6e2189dbc6edcb9ec31a9ddc70c3b5975ed
Brian <brian.welch1994@gmail.com>
2019-02-22 16:44:03 -0500

    segfault at write loop

--- datasim.c
+++ datasim.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 
 
@@ -39,16 +40,88 @@ int main(void){
 	time_t seconds = 1;
 	wiringPiISR(ackPin, INT_EDGE_FALLING, &interrupt);
 	for (;;) {
-		int xnum, xstate, ynum, ystate;
+		int xnum, ynum;
 		char xhex[16], xbin[16], yhex[16], ybin[16];
 		xnum = rand() % 500; // make this a random int in some range...
+		ynum = xnum;
 		sprintf(xhex, "%x", xnum);
-		xbin = hex2bin(xhex);
-		ybin = xbin //include y later, right now just make sure this works
+		sprintf(yhex, "%x", ynum);
+		printf("xnum %i \n", xnum);
+		printf("xhex %s \n", xhex);
+		//printf("hex2bin %s \n", hex2bin(xhex));
+		long int ix=0;
+    		while(xhex[ix]){
+    			printf("%li \n",ix);
+         		switch(xhex[ix]){
+             			case '0': strcat(xbin, "0000"); break;
+             			case '1': strcat(xbin, "0001"); break;
+             			case '2': strcat(xbin, "0010"); break;
+             			case '3': strcat(xbin, "0011"); break;
+             			case '4': strcat(xbin, "0100"); break;
+             			case '5': strcat(xbin, "0101"); break;
+             			case '6': strcat(xbin, "0110"); break;
+             			case '7': strcat(xbin, "0111"); break;
+             			case '8': strcat(xbin, "1000"); break;
+             			case '9': strcat(xbin, "1001"); break;
+             			case 'A': strcat(xbin, "1010"); break;
+             			case 'B': strcat(xbin, "1011"); break;
+             			case 'C': strcat(xbin, "1100"); break;
+             			case 'D': strcat(xbin, "1101"); break;
+             			case 'E': strcat(xbin, "1110"); break;
+             			case 'F': strcat(xbin, "1111"); break;
+             			case 'a': strcat(xbin, "1010"); break;
+             			case 'b': strcat(xbin, "1011"); break;
+             			case 'c': strcat(xbin, "1100"); break;
+             			case 'd': strcat(xbin, "1101"); break;
+             			case 'e': strcat(xbin, "1110"); break;
+             			case 'f': strcat(xbin, "1111"); break;
+             			default:  sprintf(xbin, "\nInvalid hexadecimal digit %c ",xhex[ix]); return 0;
+         		}
+         		i++;
+    		}
+		printf("x %s \n", xbin);
+		/*long int j=0;
+		printf("yhex %s \n",yhex);
+    		while(yhex[j]){
+    			printf("%li \n",j);
+         		switch(yhex[j]){
+             			case '0': strcat(ybin, "0000"); break;
+             			case '1': strcat(ybin, "0001"); break;
+             			case '2': strcat(ybin, "0010"); break;
+             			case '3': strcat(ybin, "0011"); break;
+             			case '4': strcat(ybin, "0100"); break;
+             			case '5': strcat(ybin, "0101"); break;
+             			case '6': strcat(ybin, "0110"); break;
+             			case '7': strcat(ybin, "0111"); break;
+             			case '8': strcat(ybin, "1000"); break;
+             			case '9': strcat(ybin, "1001"); break;
+             			case 'A': strcat(ybin, "1010"); break;
+             			case 'B': strcat(ybin, "1011"); break;
+             			case 'C': strcat(ybin, "1100"); break;
+             			case 'D': strcat(ybin, "1101"); break;
+             			case 'E': strcat(ybin, "1110"); break;
+             			case 'F': strcat(ybin, "1111"); break;
+             			case 'a': strcat(ybin, "1010"); break;
+             			case 'b': strcat(ybin, "1011"); break;
+             			case 'c': strcat(ybin, "1100"); break;
+             			case 'd': strcat(ybin, "1101"); break;
+             			case 'e': strcat(ybin, "1110"); break;
+             			case 'f': strcat(ybin, "1111"); break;
+             			default:  sprintf(ybin, "\nInvalid hexadecimal digit %c ",yhex[j]); return 0;
+         		}
+         		j++;
+    		}*/
+		printf("y \n");
+		//xstate = atoi(xbin);
+		//ystate = atoi(ybin);
 		for (int i=0; i < npins; i++){
-			digitalWrite(xpins[i], xstate);
-			digitalWrite(ypins[i], ystate);
-			//printf("Pin %d State is %d \n", i,xstate);
+			printf("start for loop\n");
+			int xi = atoi(xbin[i]);
+			int yi = atoi(xbin[i]);
+			printf("done atoi\n");
+			digitalWrite(xpins[i], xi);
+			digitalWrite(ypins[i], yi);
+			printf("Pin %d State is %d \n", i,xi);
 		}
 		digitalWrite(reqPin, LOW); // data available
 		// wait for signal from ACK line
@@ -92,7 +165,7 @@ int setup(void){
 	return 0;
 }
 
-char hex2bin(char input[]){
+/*char hex2bin(char input[]){
 //	const char input[] = "..."; // the value to be converted
 	char res[13]; // the length of the output string has to be n+1 where n is the number of binary digits to show, in this case 12
 	res[12] = '\0';
@@ -108,4 +181,4 @@ char hex2bin(char input[]){
 	}
 	// res now contains the binary representation of the number
 	return res;
-}
+}*/
